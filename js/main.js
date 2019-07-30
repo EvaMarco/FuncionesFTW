01bd02953062af7053616f3d6af7c20079239da3
Migara <evamarcod@gmail.com>
2019-07-30 09:59:03 +0200

    Modify functionality to accept objetcs

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ function writeThis(str, maxNumber){
 writeThis('patata', 10);
 writeThis('aguacate', 7);
 writeThis('pizza', 50);
+
+function writeThisOb(paco){
+    for (let i = 0; i < paco.max ; i++){
+        console.log(i, paco.text);
+    }
+}
+
+writeThisOb({text:'Patata', max:10});
+
+const obj = {
+    text: 'Aguacate',
+    max: 10
+}
+
+writeThisOb(obj);
